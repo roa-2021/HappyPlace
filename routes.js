b4c1af4d2b883d24a544a122f38f73d8761c21fd
matthew-hocking <matthocking2439@gmail.com>
2021-08-20 15:17:32 +1200

    basic page routes functioning

--- routes.js
+++ routes.js
@@ -1,43 +1,25 @@
 const express = require('express')
 const router = express.Router()
 
-// const db = require('./db')
+const db = require('./db')
 
 router.get('/', (req, res) => {
     //res.send("home page")
    res.render('home')
 })
 
-router.post('/choose-room', (req, res) => {
-  console.log(req.body)
-    res.render('choose-room')
-})
-
-
-
-
-// router.get('/groups', (req, res) => {
-//     db.getGroups()
-//       .then(groups => {
-//         res.render('groups', { groups: groups })
-//         return null
-//       })
-//       .catch(err => {
-//         res.status(500).send('DATABASE ERROR: ' + err.message)
-//       })
-// })
-
-router.get('/booking-summary', (req, res) => {
+router.post('/booking-summary', (req, res) => {
+  console.log("/booking-summary", req.body)
   res.render('booking-summary')
 })
 
-// router.get('/choose-room'', (req, res) => {
-//     db.getAllAreas()
-//       .then((areas) => {
-//         console.log(areas)
-//         res.render('choose-room', { areas })
-//       })
-//       .catch(err => res.status(500).send('Oh no! An error: ' + err.message))
-//   })
+router.post('/choose-room', (req, res) => {
+  console.log("/choose-room", req.body)
+  db.getAreas()
+      .then((areas) => {
+        res.render('choose-room', { areas })
+      })
+      .catch(err => res.status(500).send('Oh no! An error: ' + err.message))
+  })
 
 module.exports = router
