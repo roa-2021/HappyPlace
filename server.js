afa345f6e7a2632f129a1434abfb8c52da82b9f7
Jordan Cooper <jordan@Jordans-MacBook-Air.local>
2021-08-22 12:07:55 +1200

    add missing server config

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const hbs  = require('express-handlebars')
 
-//if you are going to add routes do it in routes.js
 const routes = require('./routes')
-
 
 const server = express()
 
-server.use(express.static('public'))
 server.engine('hbs', hbs({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
+server.use(express.urlencoded({ extended: true }))
+server.use(express.static('public'))
 
 server.use('/', routes)
 
