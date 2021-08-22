7b2d4ee5fa10200aa235faf3e9c3a917a7ef9f06
Jordan Cooper <jordan@Jordans-MacBook-Air.local>
2021-08-22 12:09:46 +1200

    cleaning up db functions/formatting

--- db.js
+++ db.js
@@ -3,42 +3,26 @@ const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
 
-let getGroups = (db = connection) => {
+const getGroups = (db = connection) => {
   return db('groups')
     .select('*', 'students.id as student_id')
     .join('students', 'students.group_id', 'groups.id')
 }
 
-let getAreas = (db = connection) => {
+const getAreas = (db = connection) => {
   return db('areas').select()
 }
 
 
-let makeBooking = (area_ID, group_ID, db = connection) => {
+const makeBooking = (area_ID, group_ID, db = connection) => {
   const newBooking = {
     area_id: area_ID,
     group_id: group_ID
   }
-  return db('bookings').insert(newBooking)
+  
+  return db('bookings')
+    .insert(newBooking)
 }
-
-// getBookings
-
-// getGroupMemebers
-// let getGroupMembers = (groupID, db = connection) => {
-//   return db('groups')
-//     .select('*', 'groups.id as id')
-//     .where('id', groupID)
-
-//     // get groups
-//     // find right group
-//     // join members
-//     // return object with groupName and members maybe as array?
-// }
-
-
-
-// getGroupMembers(50005)
 
 
 
